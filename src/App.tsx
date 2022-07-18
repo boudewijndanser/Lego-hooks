160241e437ff11450b10d072e2b35ec1c1248e10
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2022-07-18 23:24:30 +0200

    The initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+import { Button, Stack } from '@mui/material';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Badge badgeContent={4} color="primary">
+      <MailIcon color="action" />
+      </Badge>
+      <Stack direction="row" spacing={2}>
+      <Button variant="outlined">Primary</Button>
+      <Button variant="outlined" disabled>
+        Disabled
+      </Button>
+      <Button variant="outlined" href="#outlined-buttons">
+        Link
+      </Button>
+    </Stack>
+
+    <Stack direction="row" spacing={2}>
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
+    </Stack>
       </header>
     </div>
   );
