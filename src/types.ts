2bfc8ed7c8382c6b39ff0ec5058c7a6cd931bfeb
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2022-07-21 08:30:30 +0200

    Add API response types for requesting all themes

--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,17 @@ export type Form = {
     password: string
     apiKey: string
 }
+
+export type Theme = {
+    theme: string
+    setCount: number
+    subThemeCount: number
+    yearFrom: number
+    yearTo: number
+}
+
+export type ThemeApiResponse =  {
+    status: "success" // Add other status options
+    "matches": number
+    "themes": Theme[]
+}
