84fe8aa4940ce1049650f34620052fa7a028837c
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2022-07-28 00:02:49 +0200

    Add useEffect for filterValues + add theme filter to overview

--- src/components/setsOverview.tsx
+++ src/components/setsOverview.tsx
@@ -7,24 +7,60 @@ import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
-import * as React from 'react'
+import { useEffect, useState }from 'react'
 import { Set } from '../types'
 
 export const SetsOverview = ():JSX.Element => {
-    // let counted: string[] = countElementsInArray(getYearsFromSets(tmpData))
+
     let uniqueYears: number [] = getValuesFromSets<number>(tmpData,'year').filter((x, i, a) => a.indexOf(x) == i).sort((a,b) => a - b)
     let uniqueThemes: string [] = getValuesFromSets<string>(tmpData,'theme').filter((x, i, a) => a.indexOf(x) == i).sort()
     
-    const [selectedYear, setSelectedYear] = React.useState<string | undefined>('')
-    const [selectedTheme, setSelectedTheme] = React.useState<string | undefined>('')
+    const [selectedYear, setSelectedYear] = useState<string | undefined>('')
+    const [selectedTheme, setSelectedTheme] = useState<string | undefined>('')
+    const [filteredData, setFilteredData] = useState<Set[] | []>(tmpData)
 
-    let filtered: Set[] = selectedYear === ''
-        ? tmpData
-        : tmpData.filter(item => item.year.toString() === selectedYear)
+    useEffect(() => {
+        if (selectedYear !== '' && selectedTheme !== ''){
+            console.log('both selected')
+            setFilteredData(filteredData.filter(item => item.year.toString() === selectedYear))
+        } 
 
-  const handleChange = (event: SelectChangeEvent) => {
+        if (selectedYear !== ''){
+            console.log('--> Only year')
+            setFilteredData(filteredData.filter(item => item.year.toString() === selectedYear))
+        }
+
+    },[selectedYear])
+
+    useEffect(() => {
+        if (selectedTheme !== '' && selectedYear !== ''){
+            setFilteredData(filteredData.filter(item => item.theme === selectedTheme))
+        }
+
+        if (selectedTheme !== ''){
+            console.log('--> Only theme')
+            setFilteredData(filteredData.filter(item => item.theme === selectedTheme))
+        }
+
+
+    },[selectedTheme])
+
+    useEffect(() => {
+        if (selectedTheme === '' && selectedYear === ''){
+            console.log('---> Reset!')
+            setFilteredData(tmpData)
+        }
+
+    },[selectedTheme, selectedYear])
+
+
+  const handleYearChange = (event: SelectChangeEvent) => {
     setSelectedYear(event.target.value as string)
-  };
+  }
+
+  const handleThemeChange = (event: SelectChangeEvent) => {
+    setSelectedTheme(event.target.value as string)
+  }
 
   getValuesFromSets<string>(tmpData, '')
 
@@ -38,7 +74,7 @@ export const SetsOverview = ():JSX.Element => {
           id="demo-simple-select"
           value={selectedYear|| ''}
           label="Year"
-          onChange={handleChange}
+          onChange={handleYearChange}
         >
             <MenuItem value={''}>{'All'}</MenuItem> 
           {
@@ -52,12 +88,34 @@ export const SetsOverview = ():JSX.Element => {
           
         </Select>
       </FormControl>
+
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label-2">Theme</InputLabel>
+        <Select
+          labelId="demo-simple-select-label-3"
+          id="demo-simple-select-2"
+          value={selectedTheme || ''}
+          label="Theme"
+          onChange={handleThemeChange}
+        >
+            <MenuItem value={''}>{'All'}</MenuItem> 
+          {
+            uniqueThemes.map((item) => {
+                return(
+                    <MenuItem key={item.toString()} value={item.toString()}>{item}</MenuItem> 
+                )
+            })
+          }
+
+          
+        </Select>
+      </FormControl>
     </Box>
         <Box sx={{ width: 1200, height: 700, overflowY: 'scroll' }}>
-            <p>Total: {filtered.length}</p>
+            <p>Total: {filteredData.length}</p>
       <ImageList variant="masonry" cols={3} gap={8}>
             {
-                filtered.map((item) => {
+                filteredData.map((item) => {
                     return(
                     OverviewCard(item.name, item.year, item.image.thumbnailURL))
                 })
