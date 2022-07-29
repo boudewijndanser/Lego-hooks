1ffddddf5e4535e3dc32d2451f084b0247edd8b3
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2022-07-30 00:27:07 +0200

    Update loader with useEffect and material UI loader animation

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,21 +1,28 @@
 import { useUserContext } from "../context/useUserContext"
 import { useAsync } from "../hooks/useAsync"
 import { FetchData } from "./api"
+import CircularProgress from '@mui/material/CircularProgress'
+import { useEffect } from 'react';
 
 
 
 export const Loader = ():JSX.Element => {
+    
   const { user, setUser } = useUserContext();
   const { execute, status, value, error } = useAsync<string>(FetchData,false);
+
+    useEffect(()=> {
+        if (user.kind === 'readyToConnect') {
+            execute()
+        }
+    },[user])
+
+
     return(
         <>
             {
-                <p>{user.kind}</p>
-            }
-        
-            {
                 status === "pending" && 
-                <p>Loading...</p>
+                <CircularProgress color="success" />
             }
             {
                 status === "success" && 
@@ -24,10 +31,6 @@ export const Loader = ():JSX.Element => {
             {
                 status === "error" && 
                 <div>{error}</div>
-            }
-            {
-                user.kind == 'readyToConnect' &&
-                execute
             }
         </>
     )
